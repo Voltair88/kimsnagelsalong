0b4451da69246f49b3b191a84632289efc843e3e
Mattias Gulec <94360598+Gulec88@users.noreply.github.com>
2022-08-09 19:16:23 +0200

    converted to typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Container } from './styles';
+function App() {
+  return (
+    <div className='App'>
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,5 @@
+import tw from "tailwind-styled-components"
+
+const Container = tw.div`flex flex-col justify-center items-center`
+
+export { Container }
