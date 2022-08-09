d2b15c95b0634f234ad17ad00fb9647fadd5faf3
Mattias Gulec <94360598+Gulec88@users.noreply.github.com>
2022-08-09 20:21:26 +0200

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import {  SubmittButton } from './styles';
+import {  Container, SubmittButton } from './styles';
 function App() {
   return (
     <div className='App'>
+      <Container>
         <h1>Hello World</h1>
         <SubmittButton $primary={true}>Primary</SubmittButton>
+        </Container>
     </div>
   );
 }
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,13 +1,24 @@
 import styled from 'styled-components'
 
 
-const SubmittButton = styled.button`
+export const Container = styled.div`
+    background-color: #fff;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    font-size: calc(10px + 2vmin);
+    color: #1a1a1a;
+`
+
+export const SubmittButton = styled.button`
     background: ${(p: { $primary: any; }) => p.$primary ? "indigo" : "red"};
     color: white;
     border: none;
     padding: 0.5rem 1rem;   
     border-radius: 5px;
-    font-size: 1.2rem;
+    font-size: calc(8px + 2vmin);
     font-weight: bold;
     cursor: pointer;
     transition: all 0.2s ease-in-out;   
@@ -16,5 +27,3 @@ const SubmittButton = styled.button`
         color: white;
     }
 `;
-
-export { SubmittButton };
